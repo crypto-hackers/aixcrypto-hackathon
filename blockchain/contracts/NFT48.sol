c2682a39441a2c7a0eb369845d83c655ff72b540
Tomoyuki Yamasaki <tomoyukiyamasaki@Tomoyukis-MacBook-Pro.local>
2023-06-15 22:00:53 +0900

    deployed test contract

--- blockchain/contracts/NFT48.sol
+++ blockchain/contracts/NFT48.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract NFT48 is ERC721URIStorage, Ownable {
     uint256 private _tokenIdCounter;
 
     constructor() ERC721("Non Fungible Token 48", "NFT48") {
--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,6 +1,26 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: "../.env" });
+const{MUMBAI_ALCHEMY_KEY, POLYGON_ALCHEMY_KEY,MUMBAI_RPC,POLYGON_RPC,PRIVATE_KEY,POLYGONSCAN_KEY,MUMBAI_ENV} = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {
+
+    },
+    mumbai: {
+      url: `${MUMBAI_RPC}${MUMBAI_ALCHEMY_KEY}`,
+      //url: `${MUMBAI_ENV}`,
+      accounts: [PRIVATE_KEY]
+    },
+    polygon: {
+      url: `${POLYGON_RPC}${POLYGON_ALCHEMY_KEY}`,
+
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  etherscan:{
+    apiKey : POLYGONSCAN_KEY
+  }
 };
